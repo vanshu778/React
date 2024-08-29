3cb4da7352689fbf47564a8f7f7a70accf5bb1fd
vanshu778 <vthesiya778@gmail.com>
2024-08-29 20:36:17 +0530

    create react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+  return (
+    <>
+      <h1> chai aur react with vite</h1>
+      <Chai/>
+      <p>test para</p>
+    </>
+  
+  )
+}
+
+export default App
